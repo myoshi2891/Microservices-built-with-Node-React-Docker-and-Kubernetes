f65b4601e33f2fdb11bab161aa151583766ef8b8
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-04 22:25:15 +0900

    The Signin Flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Response, Request } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-	res.send("hi there!");
-});
+router.post(
+	"/api/users/signin",
+	[
+		body("email").isEmail().withMessage("Email must be valid"),
+		body("password")
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.notEmpty()
+			.withMessage("You must supply a password"),
+	],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			throw new RequestValidationError(errors.array());
+		}
+	}
+);
 
 export { router as signinRouter };
