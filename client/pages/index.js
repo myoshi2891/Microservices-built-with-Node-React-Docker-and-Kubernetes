93ba596402bdd059d560154dd0c8176dcbed2ebc
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-09 21:28:22 +0900

    Specifying the Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-
 	console.log(currentUser);
-	
+
 	return <h1>Landing Page!!</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-	const response = await axios.get("/api/users/currentuser").catch((err) => {
-		console.log(err.message);
-	});
+	if (typeof window === "undefined") {
+		const { data } = await axios.get(
+			"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+			{
+				headers: {
+					Host: "ticketing.dev",
+				},
+			}
+		);
 
-	return response.data
+		return data;
+	} else {
+		const { data } = await axios.get("/api/users/currentuser");
+
+		return data;
+	}
+
+	return {};
 };
 
 export default LandingPage;
