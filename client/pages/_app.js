a25f1e24e98af74a36570a1ed259b5adb0fdb468
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-10 12:17:54 +0900

    Moving GetInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,14 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-export default ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+const AppComponent = ({ Component, pageProps }) => {
+	return (
+		<div>
+			<h1>Header</h1>
+			<Component {...pageProps} />
+		</div>
+	);
 };
+
+AppComponent.getInitialProps = () => {};
+
+export default AppComponent;
