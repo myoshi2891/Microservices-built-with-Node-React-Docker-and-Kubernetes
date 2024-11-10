2f3027ff227c9a8303c3c89eda05b0467bea900d
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-10 12:42:16 +0900

    Passing Props Through

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,9 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import buildClient from "../api/build-client";
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
 	return (
 		<div>
-			<h1>Header</h1>
+			<h1>Header {currentUser.email}</h1>
 			<Component {...pageProps} />
 		</div>
 	);
@@ -14,14 +14,14 @@ AppComponent.getInitialProps = async (appContext) => {
 	const { data } = await client.get("/api/users/currentuser");
 
 	let pageProps = {};
-
 	if (appContext.Component.getInitialProps) {
 		pageProps = await appContext.Component.getInitialProps(appContext.ctx);
 	}
 
-	console.log(pageProps);
-
-	return data;
+	return {
+		pageProps,
+		...data,
+	};
 };
 
 export default AppComponent;
