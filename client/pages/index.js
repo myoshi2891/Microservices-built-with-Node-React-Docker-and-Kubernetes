474044badb54dc46281641db8d385316fc086b1b
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-10 12:27:12 +0900

    Issues with Custom App GetInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,6 +8,8 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
+	console.log("landing page");
+	
 	const client = buildClient(context);
 	const { data } = await client.get("/api/users/currentuser");
 	return data;
