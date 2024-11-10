b8b944ff2d1a3093015ab517f39f490613f8a478
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-10 15:56:37 +0900

    Project Setup

---
+++ tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@myoshizumitickets/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== "test",
+	})
+);
+
+app.all("*", async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+
+declare global {
+	var signin: () => Promise<string[]>;
+}
+
+let mongo: any;
+beforeAll(async () => {
+	process.env.JWT_KEY = "testenv";
+	mongo = await MongoMemoryServer.create();
+	const mongoUri = mongo.getUri();
+
+	await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+	if (mongoose.connection.db) {
+		const collections = await mongoose.connection.db.collections();
+		for (let collection of collections) {
+			await collection.deleteMany({});
+		}
+	}
+});
+
+afterAll(async () => {
+	if (mongo) {
+		await mongo.stop();
+	}
+	await mongoose.connection.close();
+});
+
+global.signin = async () => {
+	const email = "user@example.com";
+	const password = "password";
+
+	const response = await request(app)
+		.post("/api/users/signup")
+		.send({ email, password })
+		.expect(201);
+
+	const cookie = response.get("Set-Cookie");
+
+	if (!cookie) {
+		throw new Error("Failed to get cookie from response");
+	}
+
+	return cookie;
+};
