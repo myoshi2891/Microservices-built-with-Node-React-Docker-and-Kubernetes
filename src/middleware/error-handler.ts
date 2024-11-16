b57b93e828885edc2b47087ed75553b2b05c5fd6
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-16 16:25:03 +0900

    Updates

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -12,6 +12,8 @@ export const errorHandler = (
 			.send({ errors: err.serializeErrors() });
 	}
 
+	console.error(err);
+
 	res.status(400).send({
 		errors: [{ message: "Something went wrong..." }],
 	});
