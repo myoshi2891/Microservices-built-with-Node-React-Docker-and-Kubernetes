983a45ddf3c440cf6cc22105cffdcde8f4f61262
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-16 15:27:22 +0900

    Reminder on Mongoose with TypeScript

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -48,4 +48,9 @@ it("returns an error if an invalid price is provided", async () => {
 		.send({ title: "test 10" })
 		.expect(400);
 });
-// it("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+	await request(app)
+		.post("/api/tickets")
+		.send({ title: "test", price: 20 })
+		.expect(201);
+});
