e4ccd282322dd29723b37165ab9f0c4f0a4ed12a
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-16 17:33:02 +0900

    Permission Checking

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -23,6 +23,23 @@ it("returns a 401 if the user is not authenticated", async () => {
 		})
 		.expect(401);
 });
-it("returns a 401 if the user does not own the ticket", async () => {});
-it("returns a 400 if the user provides an invalide title or price", async () => {});
-it("updates the ticket provided valid input", async () => {});
+it("returns a 401 if the user does not own the ticket", async () => {
+	const response = await request(app)
+		.post("/api/tickets")
+		.set("Cookie", global.signin())
+		.send({
+			title: "concert",
+			price: 20,
+		});
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set("Cookie", global.signin())
+		.send({
+			title: "updated title",
+			price: 25,
+		})
+		.expect(401);
+});
+// it("returns a 400 if the user provides an invalide title or price", async () => {});
+// it("updates the ticket provided valid input", async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -36,7 +36,7 @@ afterAll(async () => {
 global.signin = () => {
 	// Build a JWT payload. { id, email }
 	const payload = {
-		id: "testojopj",
+		id: new mongoose.Types.ObjectId().toHexString(),
 		email: "test@test.com",
 	};
 
