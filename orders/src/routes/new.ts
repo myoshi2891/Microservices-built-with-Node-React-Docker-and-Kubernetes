0e2caf96b9add37943073f3dfd11852230af303c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-11-24 16:35:38 +0900

    Finding Reserved Tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import {
 	NotFoundError,
 	requireAuth,
 	validateRequest,
+	OrderStatus,
+	BadRequestError,
 } from "@myoshizumitickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
@@ -32,6 +34,21 @@ router.post(
 		}
 
 		// Make sure that this ticket is not already reserved
+		// Run Query to look at all orders. Find an order where the ticket is the ticket we just found *and* the orders status is *not* cancelled. if we find an order from that means the ticket *is* reserved
+		const existingOrder = await Order.findOne({
+			ticket: ticket,
+			status: {
+				$in: [
+					OrderStatus.Created,
+					OrderStatus.AwaitingPayment,
+					OrderStatus.Complete,
+				],
+			},
+		});
+
+		if (existingOrder) {
+			throw new BadRequestError("Ticket is already reserved");
+		}
 
 		// Calculate an expiration date for this order
 
