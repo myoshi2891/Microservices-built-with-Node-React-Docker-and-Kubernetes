09fa99a381ab112825a04f10a90e28bdf0e2f02a
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-01 16:54:02 +0900

    Blueprint for Listeners

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,14 @@
+import { Message } from "node-nats-streaming";
+import {
+	Subjects,
+	Listener,
+	TicketCreatedEvent,
+} from "@myoshizumitickets/common";
+
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	subject: Subjects.TicketCreated = Subjects.TicketCreated;
+	queueGroupName = "orders-service";
+	onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+}
