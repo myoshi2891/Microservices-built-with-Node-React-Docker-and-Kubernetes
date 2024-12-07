d266e5cdffdd85ab6f997f9a1be9756f15922760
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-07 17:06:05 +0900

    Testing the Ack Call

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -39,6 +39,9 @@ it("creates and saves a ticket", async () => {
 });
 
 it("acks the message", async () => {
+    const { data, listener, msg } = await setup();
 	// call the onMessage function with the data object + message object
+	await listener.onMessage(data, msg);
 	// write assertion to make sure ack function is called
+	expect(msg.ack).toHaveBeenCalled();
 });
