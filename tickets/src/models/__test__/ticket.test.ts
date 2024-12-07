45b0c84d85f4f84a4ab544a82b1e6c6a99d3cd6b
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-07 13:22:44 +0900

    Testing OCC

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,3 +1,32 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async () => {});
+it("implements optimistic concurrency control", async () => {
+	// Create an instance of ticket
+	const ticket = Ticket.build({
+		title: "concert",
+		price: 20,
+		userId: "testuser",
+	});
+	// Save the ticket to the database
+	await ticket.save();
+
+	// fetch the ticket twice
+	const firstInstance = await Ticket.findById(ticket.id);
+	const secondInstance = await Ticket.findById(ticket.id);
+
+	// make two seprate changes to the tickets we fetched
+	firstInstance!.set({ price: 10 });
+	secondInstance!.set({ price: 15 });
+
+	// save the first fetched ticket
+	await firstInstance!.save();
+
+	// save the second fetched ticket and expect an error
+	try {
+		await secondInstance!.save();
+	} catch (err) {
+		return;
+	}
+
+	throw new Error("Should not reach this point");
+});
