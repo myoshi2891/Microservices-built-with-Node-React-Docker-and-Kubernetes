c7123f4f50c5d5607321456274c3e9c75d4b9eff
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-07 17:26:18 +0900

    Success Case Testing

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -34,6 +34,22 @@ const setup = async () => {
 	return { listener, ticket, data, msg };
 };
 
-it("finds, updates, and saves a ticket", async () => {});
+it("finds, updates, and saves a ticket", async () => {
+	const { msg, data, ticket, listener } = await setup();
 
-it("acks the message", async () => {});
+	await listener.onMessage(data, msg);
+
+	const updatedTicket = await Ticket.findById(ticket.id);
+
+	expect(updatedTicket!.title).toEqual(data.title);
+	expect(updatedTicket!.price).toEqual(data.price);
+	expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it("acks the message", async () => {
+	const { msg, data, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
