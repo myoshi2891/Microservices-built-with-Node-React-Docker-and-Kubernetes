ff84defe21c31ee43414975d22504e31854e78e2
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-15 13:36:23 +0900

    Validating Order Payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,7 +4,9 @@ import {
 	requireAuth,
 	validateRequest,
 	NotFoundError,
+	NotAuthorizedError,
 	BadRequestError,
+	OrderStatus,
 } from "@myoshizumitickets/common";
 import { Order } from "../models/order";
 
@@ -16,7 +18,23 @@ router.post(
 	[body("token").not().isEmpty(), body("orderId").not().isEmpty()],
 	validateRequest,
 	async (req: Request, res: Response) => {
-		res.send({ success: true });
+        const { token, orderId } = req.body;
+
+		const order = await Order.findById(orderId);
+
+		if (!order) {
+			throw new NotFoundError();
+		}
+
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+
+		if (order.status === OrderStatus.Cancelled) {
+			throw new BadRequestError("Cannot pay for an cancelled order.");
+		}
+
+		res.send({ sucess: true });
 	}
 );
 
