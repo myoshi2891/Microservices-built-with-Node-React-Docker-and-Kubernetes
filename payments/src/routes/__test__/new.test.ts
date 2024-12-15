a47d83000455d74e8392e1b9e669ffa2eadb6337
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-15 15:39:13 +0900

    Automated Payment Testing

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@myoshizumitickets/common";
 
+jest.mock("../../stripe");
+
 it("returns a 404 when purchasing an order that does not exist", async () => {
 	await request(app)
 		.post("/api/payments")
@@ -57,4 +59,26 @@ it("returns a 400 when purchasing a cancelled order", async () => {
 			token: "lkjhgea",
 		})
 		.expect(400);
+});
+
+it("returns a 204 with valid inputs", async () => {
+	const userId = new mongoose.Types.ObjectId().toHexString();
+
+	const order = Order.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
+		userId: new mongoose.Types.ObjectId().toHexString(),
+		version: 0,
+		price: 20,
+		status: OrderStatus.Created,
+	});
+
+	await order.save();
+
+	await request(app)
+		.post("/api/payments")
+		.set("Cookie", global.signin(userId))
+		.send({
+			token: "tok_visa",
+			orderId: order.id,
+		});
 });
