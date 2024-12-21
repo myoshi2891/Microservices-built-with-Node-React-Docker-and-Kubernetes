9b2d972bea796a4d54ac95ed092600d08eb5a99b
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-21 18:09:25 +0900

    Paying for an Order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order, currentUser }) => {
 	const [timeLeft, setTimeLeft] = useState(0);
+	const { doRequest, errors } = useRequest({
+		url: "/api/payments",
+		method: "post",
+		body: {
+			orderId: order.id,
+		},
+		onSuccess: (payment) => console.log(payment),
+	});
+
 	const stripeKey = process.env.NEXT_PUBLIC_STRIPE_KEY;
 	useEffect(() => {
 		const findTimeLeft = () => {
@@ -26,11 +36,12 @@ const OrderShow = ({ order, currentUser }) => {
 		<div>
 			Time left to pay: {timeLeft} seconds
 			<StripeCheckout
-				token={(token) => console.log(token)}
+				token={({ id }) => doRequest({ token: id })}
 				stripeKey={stripeKey}
 				amount={order.ticket.price * 100}
 				email={currentUser.email}
 			/>
+			{errors}
 		</div>
 	);
 };
